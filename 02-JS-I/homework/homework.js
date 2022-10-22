a84ed6df4687e5bbb8ac422fa4daec802a56a117
JhossimarCarlos <jhossimarcarlos@gmail.com>
2022-10-22 12:47:56 -0500

    estamos ready

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Habla pe bateria';
+const nuevaString = 'Habla pe barrio';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
